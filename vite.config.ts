ae02be54b140e32b66160c18337b0ca967d301bb
Luiz Silva <lfmachadodasilva@gmail.com>
2025-10-05 17:47:48 +0100

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cotoco/',
+  base: './',
   root: './src',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
